030d9eb8054741c63c5885865c0f9522c888b520
BW Oh <bwoh@outlook.kr>
2021-01-21 18:23:17 +0000

    🚚 Create database folder

--- database/gunpladb.sql
+++ database/gunpladb.sql
@@ -1,3 +1,9 @@
+grant all privileges on  *.* to 'root'@'%' identified by 'mysql1234';
+delete from mysql.user where host="localhost" and user="root";
+flush privileges;
+select host,user,plugin,authentication_string from mysql.user;
+
+#####
 DROP DATABASE IF EXISTS `gunpladb` ;
 
 CREATE DATABASE IF NOT EXISTS `gunpladb` 
--- src/gunpla.ts
+++ src/gunpla.ts
@@ -63,7 +63,7 @@ app.get('/select_id', (req: Request, res: Response) => {
 
 app.get('/gunpla/id', (req: Request, res: Response) => {
 	const query: string = "select * from gunpla where id = ?";
-	dbc.query(query, [ req.parans.id ], (err, rows) => {
+	dbc.query(query, [ req.params.id ], (err, rows) => {
 		if (err) return console.log(err);
 		res.send(rows);
 	})
